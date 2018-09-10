f3aa264a30c77ab1436fdea96b17735f4d7dcb31
Arturas <a.molcanovas@gmail.com>
2018-09-10 23:54:02 +0100

    prep build, rm dist

--- dist/es5/getCallback.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function getCallback() {
-    if (arguments.length && typeof arguments[arguments.length - 1] === 'function') {
-        return arguments[arguments.length - 1];
-    }
-}
-exports.getCallback = getCallback;
-//# sourceMappingURL=getCallback.js.map
