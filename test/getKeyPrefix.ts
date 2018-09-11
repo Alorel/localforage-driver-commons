ff77b351a652812f6da429a7f96a95bff4a51b03
Arturas <a.molcanovas@gmail.com>
2018-09-11 20:41:39 +0100

    tests(getKeyPrefix): Update tests for new prefix function

--- test/getKeyPrefix.ts
+++ test/getKeyPrefix.ts
@@ -5,12 +5,12 @@ describe('getKeyPrefix', () => {
   it('non-default name', () => {
     const p = getKeyPrefix({name: 'foo'}, {storeName: 'bar'});
 
-    expect(p).to.eq('foo/bar');
+    expect(p).to.eq('foo/bar/');
   });
 
   it('non-default storeName', () => {
     const p = getKeyPrefix({storeName: 'qux'}, {name: 'baz'});
 
-    expect(p).to.eq('baz/qux');
+    expect(p).to.eq('baz/qux/');
   });
 });
